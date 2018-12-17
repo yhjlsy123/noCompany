298ae078c169896d70e6921baff952c9469fe13e
yhjlsy123 <shoushuai@yeah.net>
2018-12-17 18:37:09 +0800

    追加权限申请

---
+++ hotel/src/main/java/hotel/xy/com/hotel/at/SplashActivity.java
@@ -0,0 +1,38 @@
+package hotel.xy.com.hotel.at;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+
+import hotel.xy.com.hotel.R;
+
+public class SplashActivity extends BaseActivity {
+    private static final int REQUEST_CODE = 0; // 请求码
+
+    // 所需的全部权限
+    static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.CAMERA,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        PermissionsActivity.startActivityForResult(this, REQUEST_CODE, PERMISSIONS);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 拒绝时, 关闭页面, 缺少主要权限, 无法运行
+        if (requestCode == REQUEST_CODE && resultCode == PermissionsActivity.PERMISSIONS_DENIED) {
+            finish();
+        }
+    }
+}
